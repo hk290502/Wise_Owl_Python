e44bcd2714fba3ec3664fb5167f02b132d5c3a0a
Hamza Khan <hamza.khan@envitia.com>
2026-02-09 18:46:50 +0000

    Completing basic coding exersizes

---
+++ basic_coding/slackadder4680.py
@@ -0,0 +1,7 @@
+first_user_number = int(input("Think of a number ==> "))
+second_user_number = int(input("Think of a second number ==> "))
+final_number = first_user_number + second_user_number
+print("The sum of " + str(first_user_number) + " + " + str(second_user_number) + " is " + str(final_number))
+
+#print(f"The sum of {first_user_number} + {second_user_number} is {final_number}")
+
---
+++ basic_coding/sortinghat4681.py
@@ -0,0 +1,15 @@
+sorting_hat_house_selection = str(input("What house are you in?  "))
+
+if sorting_hat_house_selection == "Gryffindor":
+    print("Your house colours are Scarlet and yellow (of course)")
+elif sorting_hat_house_selection == "Hufflepuff":
+    print("Your house colours are Yellow and black")
+elif sorting_hat_house_selection == "Ravenclaw":
+    print("Your house colours are Blue and bronze")
+elif sorting_hat_house_selection == "Slytherin":
+     print("Your house colours are Green and silver (hissss ... !)")
+else:
+    print("\n\n Not Known")
+
+
+
