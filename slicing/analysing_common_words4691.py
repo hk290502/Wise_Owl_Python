6dfcaf96f9ed72af4eed73457b7d547a4fbd4de5
Hamza Khan <hamza.khan@envitia.com>
2026-03-13 16:18:05 +0000

    title case for slicing ex2

--- slicing/analysing_common_words4691.py
+++ slicing/analysing_common_words4691.py
@@ -12,23 +12,27 @@ print(common_words)
 
 # how many are there?
 title = "A - Number of words"
+print(title)
 print(len(common_words))
 
 
 
 # show the first 5 words
 title = "B - First 5 words"
+print(title)
 print(common_words[:5])
 
 
 
 # the last 3 words
 title = "C - Last 3 words"
+print(title)
 print(common_words[-3:])
 
 
 # every fifth word
 title = "D - Every 5th word"
+print(title)
 print(common_words[::5])
 
 
@@ -36,6 +40,7 @@ print(common_words[::5])
 
 # iterate over words, listing out the ones with four letters
 title = "E - Four-letter words"
+print(title)
 for word in common_words:
     if len(word) == 4:
         print(word)
@@ -45,18 +50,17 @@ for word in common_words:
 
 
 
-# words beginning with W
 title = "F - Words starting with W"
+print(title)  # ← this is missing everywhere
 for word in common_words:
     if word.startswith("w"):
         print(word)
-
-    
 
 
 
 
 # show the words in alphabetical order (need to convert to a list first)
 title = "G - Words in order"
+print(title)
 print(sorted(common_words))
 
